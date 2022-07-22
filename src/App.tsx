f95eac496c7a043082ffde222c7e6453ccd2d74f
Filip Toma <itsfiliptoma@gmail.com>
2022-07-22 22:11:05 +0200

    Setup Mantine theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import "@fontsource/poppins/400.css" // Weight: normal
+import "@fontsource/poppins/600.css" // Weight: semibold
+import "@fontsource/poppins/700.css" // Weight: bold
+import { ColorScheme, ColorSchemeProvider, MantineProvider, Text } from "@mantine/core"
+import { useLocalStorage } from "@mantine/hooks"
+
 import "./App.css"
 
+import theme from "./utils/theme"
+
 const App = () => {
-  return <div>Hello, World!</div>
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "color-scheme",
+    defaultValue: "dark",
+    getInitialValueInEffect: true
+  })
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || colorScheme === "dark" ? "light" : "dark")
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider withGlobalStyles theme={theme}>
+        <Text>Hello, Mantine!</Text>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
 
 export default App
---
+++ src/utils/theme.ts
@@ -0,0 +1,7 @@
+import { MantineThemeOverride } from "@mantine/core"
+
+const theme: MantineThemeOverride = {
+  fontFamily: "Poppins"
+}
+
+export default theme
