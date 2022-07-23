ff0d697194434d5f234160610ccd48961e5a9a82
Filip Toma <itsfiliptoma@gmail.com>
2022-07-23 20:54:22 +0200

    Update global styles

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -6,7 +6,7 @@ const GlobalStyles = () => {
       styles={(theme) => ({
         body: {
           backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-          color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.colors.gray[9]
+          color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.colors.gray[8]
         }
       })}
     />
